4867b4923ac807e4a72ec8e5279ceb5325e6fc76
noaharagon <50531441+noaharagon@users.noreply.github.com>
2021-05-24 15:59:54 +0200

    Update finvola_draft_code.R
    update news impact curve

--- finvola_draft_code.R
+++ finvola_draft_code.R
@@ -167,9 +167,11 @@ gjrgarch_news = newsimpact(z = NULL, fit_gjrgarch)
 
 #news impact curve
 plot(gjrgarch_news$zx, gjrgarch_news$zy, ylab=gjrgarch_news$yexpr, 
-     xlab=gjrgarch_news$xexpr, type="l", main = "News Impact Curve" ,ylim=c(0,0.01))
-plot(garch_news$zx, garch_news$zy, xlab=garch_news$xexpr, ylab=garch_news$yexpr
-     , type="l", main = "News Impact Curve" ,ylim=c(0,0.01))
+     xlab=gjrgarch_news$xexpr, type="l", main = "News Impact Curve" ,ylim=c(0,0.01), lwd = 2)
+lines(garch_news$zx, garch_news$zy, xlab=garch_news$xexpr, ylab=garch_news$yexpr
+     , type="l", main = "News Impact Curve" ,ylim=c(0,0.01), lwd = 2, col = "red")
+legend(0.03, 0.008, legend=c("GARCH", "GJR-GARCH"),
+       col=c("red", "black"), lty=1, cex=0.8)
 
 # rolling forecast for Markov-Switching GARCH with forecast length of 504 days
 n.ots <- 504 #window size for forecast
